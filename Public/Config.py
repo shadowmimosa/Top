f140fd87d8997ff7de8bb15e30a3407bed48a50b
邹志权 <zouzhilu@163.com>
2018-04-16 15:40:12 +0800

    更改config。py文件，加入环境判断

--- Public/Config.py
+++ Public/Config.py
@@ -1,6 +1,23 @@
 #coding=utf-8
 
-##==================================请求头参数（开发测试生产环境通用）=======================
+import sys
+import time,traceback
+import os
+import platform
+import subprocess
+import random
+import re
+
+
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+# # 环境变量配置JoyrunEvn（测试环境值：Test或0；生产环境值：其他值为生产环境）
+JoyrunEvn='0'
+
+##==================================开发测试生产环境通用参数=======================
 #请求头参数-前端系统版本号
 SYSVERSION='11.2.1'
 
@@ -22,85 +39,15 @@ appversion='ios3.1.1'
 #请求头参数Content-Type
 #ContentType='application/x-www-form-urlencoded'
 
-# # 环境变量（Test,Online）
+#线上运行用户列表   注意以下用名的密码要一致；
+Onlineuserlist='13926281760,13926281760,5145311,13064764870,13064764870,13064764870,5145311,86121631,13829744541'
 
+#测试运行用户列表   注意以下用名的密码要一致；
+Testuserlist='13829744541,13829744542,13829744543,13829744544,13829744545,5145311'
 
-# #===============测试环境下连接地址(Test)=======================
-
-# 登录地址
-login_URL='http://api-test.thejoyrun.com' 
-
-#api项目地址
-api_URL='http://api-test.thejoyrun.com' 
-
-#beta约定跑相关
-beta_URL='http://beta-test.thejoyrun.com'
-
-#bet 动态配置相关
-bet_URL='http://bet-test.api.thejoyrun.com'
-
-# user用户工程,与用户信息相关
-user_URL='http://u-test.api.thejoyrun.com' 
-
-# topic工程,与首页相关
-topic_URL='http://topic-test.api.thejoyrun.com' 
-
-#crew-muilt 与跑团相关1
-crew_muilt_URL='http://crew-muilt-test.api.thejoyrun.com' 
-
-#crew 与跑团相关2
-crew_URL='http://crew-test.api.thejoyrun.com' 
-
-#crewapp 跑团相关3
-crewapp_URL='http://crewapp-test.api.thejoyrun.com'
-
-# ec电商工程,与订单相关
-ec_URL='http://ec-test.thejoyrun.com'
-
-# advert工程,与消息广告通告相关
-advert_URL='http://advert-test.api.thejoyrun.com'
-
-# training工程,与训练相关
-training_URL='http://training-test.api.thejoyrun.com'
-
-# wear工程,与穿戴相关
-wear_URL='http://wear-test.api.thejoyrun.com'
-
-#point_
-point_URL='http://point-test.api.thejoyrun.com'
-
-#im 消息相关
-im_URL='http://im-test.api.thejoyrun.com'
-
-#marathon 马拉松相关
-marathon_URL='http://marathon-test.api.thejoyrun.com'
-
-#recommend 推荐相关
-recommend_URL='http://recommend-test.api.thejoyrun.com'
-
-#live 赛事直播相关
-live_URL='http://live-test.api.thejoyrun.com'
-
-#wallet 推荐相关
-wallet_URL='http://wallet-test.api.thejoyrun.com'
-
-#media 媒体资讯相关
-media_URL='http://media-test.api.thejoyrun.com'
-
-#challenge 挑战相关
-challenge_URL='http://challenge-test.api.thejoyrun.com'
-
-#event 事件相关
-event_URL='http://event-test.api.thejoyrun.com'
-
-#小程序统一域名
-mapp_URL='https://mapp-test.api.thejoyrun.com'
-
-#企业跑团
-cr_URL='http://cr-test.thejoyrun.com'
-
-
-#各版本测试（3.1）密钥
+#各版本测试（3.1）密钥  （app_secrets["3.1"] = "fb1931e425f84313bfae4b93ab3ccdc4"
+#app_secrets["3.2"] = "0ce938187774429689749a80d54d4a1b"
+#app_secrets["3.3"] = "5dbfb17323a24666a6b6aa0b9620cab6"）
 appkey1='fb1931e425f84313bfae4b93ab3ccdc4'
 
 #业务测试密钥
@@ -109,137 +56,200 @@ appkey2='1fd6e28fd158406995f77727b35bf20a'
 #前端验签名的版本号（需要与appkey1相对应，服务根据这个版本号验证appkey1）
 APPVERSION='100.3.1.0'
 
-#跑团小程序密钥
-corpcrewappkey='hello-joyrun-micro-app'
-
-#跑团小程序APPID
-corpcrewappid='wx1fef2e38049c8d5f'
-
-#跑团小程序用户身份sid  2018-02-06 12:30 
-corpcrewsid='eda1412c27f249189d4a0901d9c18c4b8'
-
-#线上运行用户列表   注意以下用名的密码要一致；
-Onlineuserlist='13926281760,13926281760,5145311,13064764870,13064764870,13064764870,5145311,86121631,13829744541'
-
-#测试运行用户列表   注意以下用名的密码要一致；
-Testuserlist='13829744541,13829744542,13829744543,13829744544,13829744545,5145311'
-
-#约定跑小程序密钥
-betappkey='hello-joyrun-micro-app'
-
-#约定跑小程序APPID    wxd19597f62e33ba65
-betappid='wxd19597f62e33ba65'
-
-#约定跑小程序用户身份sid  2018-02-06 12:30 
-betsid='eda1412c27f249189d4a0901d9c18c4b8'
 
 
+if  JoyrunEvn=='Test' or JoyrunEvn=='0':
+
+
+	# #===============测试环境下连接地址(Test)=======================
+
+	# 登录地址
+	login_URL='http://api-test.thejoyrun.com' 
+
+	#api项目地址
+	api_URL='http://api-test.thejoyrun.com' 
+
+	#beta约定跑相关
+	beta_URL='http://beta-test.thejoyrun.com'
+
+	#bet 动态配置相关
+	bet_URL='http://bet-test.api.thejoyrun.com'
+
+	# user用户工程,与用户信息相关
+	user_URL='http://u-test.api.thejoyrun.com' 
+
+	# topic工程,与首页相关
+	topic_URL='http://topic-test.api.thejoyrun.com' 
+
+	#crew-muilt 与跑团相关1
+	crew_muilt_URL='http://crew-muilt-test.api.thejoyrun.com' 
+
+	#crew 与跑团相关2
+	crew_URL='http://crew-test.api.thejoyrun.com' 
+
+	#crewapp 跑团相关3
+	crewapp_URL='http://crewapp-test.api.thejoyrun.com'
+
+	# ec电商工程,与订单相关
+	ec_URL='http://ec-test.thejoyrun.com'
+
+	# advert工程,与消息广告通告相关
+	advert_URL='http://advert-test.api.thejoyrun.com'
+
+	# training工程,与训练相关
+	training_URL='http://training-test.api.thejoyrun.com'
+
+	# wear工程,与穿戴相关
+	wear_URL='http://wear-test.api.thejoyrun.com'
+
+	#point_
+	point_URL='http://point-test.api.thejoyrun.com'
+
+	#im 消息相关
+	im_URL='http://im-test.api.thejoyrun.com'
+
+	#marathon 马拉松相关
+	marathon_URL='http://marathon-test.api.thejoyrun.com'
+
+	#recommend 推荐相关
+	recommend_URL='http://recommend-test.api.thejoyrun.com'
+
+	#live 赛事直播相关
+	live_URL='http://live-test.api.thejoyrun.com'
+
+	#wallet 推荐相关
+	wallet_URL='http://wallet-test.api.thejoyrun.com'
+
+	#media 媒体资讯相关
+	media_URL='http://media-test.api.thejoyrun.com'
+
+	#challenge 挑战相关
+	challenge_URL='http://challenge-test.api.thejoyrun.com'
+
+	#event 事件相关
+	event_URL='http://event-test.api.thejoyrun.com'
+
+	#企业跑团
+	cr_URL='http://cr-test.thejoyrun.com'
+
+	##===================小程序相关配置=====================
+
+	#小程序统一域名
+	mapp_URL='https://mapp-test.api.thejoyrun.com'
+
+	#跑团小程序密钥
+	corpcrewappkey='hello-joyrun-micro-app'
+
+	#跑团小程序APPID
+	corpcrewappid='wx1fef2e38049c8d5f'
+
+	#跑团小程序用户身份sid  2018-02-06 12:30 
+	corpcrewsid='eda1412c27f249189d4a0901d9c18c4b8'
+
+	#约定跑小程序密钥
+	betappkey='hello-joyrun-micro-app'
+
+	#约定跑小程序APPID    wxd19597f62e33ba65
+	betappid='wxd19597f62e33ba65'
+
+	#约定跑小程序用户身份sid  2018-02-06 12:30 
+	betsid='eda1412c27f249189d4a0901d9c18c4b8'
+
+else:
 #=======================线上发布连接地址(Online)========================
 
+	# 登录地址
+	login_URL='http://api.thejoyrun.com' 
 
-# # 登录地址
-# login_URL='http://api.thejoyrun.com' 
+	#api项目地址
+	api_URL='http://api.thejoyrun.com' 
 
-# #api项目地址
-# api_URL='http://api.thejoyrun.com' 
+	#beta约定跑相关
+	beta_URL='http://beta.thejoyrun.com'
 
-# #beta约定跑相关
-# beta_URL='http://beta.thejoyrun.com'
+	#bet 动态配置相关
+	bet_URL='http://bet.api.thejoyrun.com'
 
-# #bet 动态配置相关
-# bet_URL='http://bet.api.thejoyrun.com'
+	# user用户工程,与用户信息相关
+	user_URL='http://u.api.thejoyrun.com' 
 
-# # user用户工程,与用户信息相关
-# user_URL='http://u.api.thejoyrun.com' 
+	# topic工程,与首页相关
+	topic_URL='http://topic.api.thejoyrun.com' 
 
-# # topic工程,与首页相关
-# topic_URL='http://topic.api.thejoyrun.com' 
+	#crew-muilt 与跑团相关1
+	crew_muilt_URL='http://crew-muilt.api.thejoyrun.com' 
 
-# #crew-muilt 与跑团相关1
-# crew_muilt_URL='http://crew-muilt.api.thejoyrun.com' 
+	#crew 与跑团相关2
+	crew_URL='http://crew.api.thejoyrun.com' 
 
-# #crew 与跑团相关2
-# crew_URL='http://crew.api.thejoyrun.com' 
+	#crewapp 跑团相关3
+	crewapp_URL='http://crewapp.api.thejoyrun.com'
 
-# #crewapp 跑团相关3
-# crewapp_URL='http://crewapp.api.thejoyrun.com'
+	# ec电商工程,与订单相关
+	ec_URL='http://ec.thejoyrun.com'
 
-# # ec电商工程,与订单相关
-# ec_URL='http://ec.thejoyrun.com'
+	# advert工程,与消息广告通告相关
+	advert_URL='http://advert.api.thejoyrun.com'
 
-# # advert工程,与消息广告通告相关
-# advert_URL='http://advert.api.thejoyrun.com'
+	# training工程,与训练相关
+	training_URL='http://training.api.thejoyrun.com'
 
-# # training工程,与训练相关
-# training_URL='http://training.api.thejoyrun.com'
+	# wear工程,与穿戴相关
+	wear_URL='http://wear.api.thejoyrun.com'
 
-# # wear工程,与穿戴相关
-# wear_URL='http://wear.api.thejoyrun.com'
+	#point_
+	point_URL='http://point.api.thejoyrun.com'
 
-# #point_
-# point_URL='http://point.api.thejoyrun.com'
+	#im 消息相关
+	im_URL='http://im.api.thejoyrun.com'
 
-# #im 消息相关
-# im_URL='http://im.api.thejoyrun.com'
+	#marathon 马拉松相关
+	marathon_URL='http://marathon.api.thejoyrun.com'
 
-# #marathon 马拉松相关
-# marathon_URL='http://marathon.api.thejoyrun.com'
+	#recommend 推荐相关
+	recommend_URL='http://recommend.api.thejoyrun.com'
 
-# #recommend 推荐相关
-# recommend_URL='http://recommend.api.thejoyrun.com'
+	#live 赛事直播相关
+	live_URL='http://live.api.thejoyrun.com'
 
-# #live 赛事直播相关
-# live_URL='http://live.api.thejoyrun.com'
+	#wallet 推荐相关
+	wallet_URL='http://wallet.api.thejoyrun.com'
 
-# #wallet 推荐相关
-# wallet_URL='http://wallet.api.thejoyrun.com'
+	#media 媒体资讯相关
+	media_URL='http://media.api.thejoyrun.com'
 
-# #media 媒体资讯相关
-# media_URL='http://media.api.thejoyrun.com'
+	#challenge 挑战相关
+	challenge_URL='http://challenge.api.thejoyrun.com'
 
-# #challenge 挑战相关
-# challenge_URL='http://challenge.api.thejoyrun.com'
+	#event 事件相关
+	event_URL='http://event.api.thejoyrun.com'
 
-# #event 事件相关
-# event_URL='http://event.api.thejoyrun.com'
+	#企业跑团
+	cr_URL='https://cr.thejoyrun.com'
 
-# #小程序统一域名
-# mapp_URL='https://mapp.api.thejoyrun.com'
+	#=====小程序相关配置=================================
 
-# #企业跑团
-# cr_URL='https://cr.thejoyrun.com'
+	#小程序统一域名
+	mapp_URL='https://mapp.api.thejoyrun.com'
+	
 
-# #各版本测试（3.1）密钥
-# appkey1='fb1931e425f84313bfae4b93ab3ccdc4'
+	#跑团小程序密钥
+	corpcrewappkey='0a7d4aa5-c13c-40c2-9b1e-3476071a6f82'
 
-# #业务测试密钥
-# appkey2='1fd6e28fd158406995f77727b35bf20a'
+	#跑团小程序APPID
+	corpcrewappid='wx24fffb22401a1157'
 
-# #前端验签名的版本号（需要与appkey1相对应，服务根据这个版本号验证appkey1）
-# APPVERSION='100.3.1.0'
+	#跑团小程序用户身份sid===2018-02-06  10:28
+	corpcrewsid='6861e3f7620840c2b03d8eff181a61d70'
 
-# #跑团小程序密钥
-# corpcrewappkey='0a7d4aa5-c13c-40c2-9b1e-3476071a6f82'
+	##约定跑小程序密钥
+	betappkey='3e3aee29-6abb-495a-bb74-512acda2979e'
 
-# #跑团小程序APPID
-# corpcrewappid='wx24fffb22401a1157'
+	##约定跑小程序APPID
+	betappid='wxd19597f62e33ba65'
 
-# #跑团小程序用户身份sid===2018-02-06  10:28
-# corpcrewsid='6861e3f7620840c2b03d8eff181a61d70'
-
-# #线上运行用户列表   注意以下用名的密码要一致；
-# Onlineuserlist='13829744541,13926281760,5145311'
-
-# #测试运行用户列表   注意以下用名的密码要一致；
-#Testuserlist='13829744541,13829744542,13829744543,13829744544,13829744545,5145311'
-#约定跑小程序密钥
-#betappkey='3e3aee29-6abb-495a-bb74-512acda2979e'
-
-#约定跑小程序APPID
-#betappid='wxd19597f62e33ba65'
-
-#约定跑小程序用户身份sid  2018-02-06 12:30 
-#betsid='eda1412c27f249189d4a0901d9c18c4b8'
+	##约定跑小程序用户身份sid  2018-02-06 12:30 
+	betsid='6861e3f7620840c2b03d8eff181a61d70'
 
 
 
