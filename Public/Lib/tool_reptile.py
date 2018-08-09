13c9e2b8e56b3cbb2add28f260bf0e22e7dc9efc
邹志权 <zouzhilu@163.com>
2018-08-09 16:11:35 +0800

    增加 sid过期重试3次功能

--- Public/Lib/tool_reptile.py
+++ Public/Lib/tool_reptile.py
@@ -1,15 +1,35 @@
-# coding=utf-8
+# coding=UTF-8
 import re
 import sys
 import json
 
 reload(sys)
-sys.setdefaultencoding('utf-8')
+sys.setdefaultencoding('UTF-8')
 
 
 #class tools_reptile():
 #    ''' It is reptile;信息清洗方法集合
 #     '''
+def convertCode(dataobj,encodeingtype,datatypes=None):
+    if datatypes in ["List","list","LIST",None]:
+        try:
+            dataobj1 = json.dumps(dataobj,ensure_ascii=False,encoding=encodeingtype)
+            print  dataobj1
+            print  type(dataobj1)
+            print "完成转码".decode('UTF-8')
+            dataobj = json.loads(dataobj1)
+            print  type(dataobj)
+        except UnicodeEncodeError:
+            try:
+                dataobj = json.dumps(dataobj, ensure_ascii=False, encoding=encodeingtype)
+                print  "未完成转码".decode('UTF-8')
+            except:
+                pass
+    print   "dataobj类型为",type(dataobj)
+    return  dataobj
+
+
+
 
 def FindAndValue(yuanstring, leftlimit, rightlimit, ORD=None, valuetype=None, pt=None):
     '''
@@ -33,29 +53,57 @@ def FindAndValue(yuanstring, leftlimit, rightlimit, ORD=None, valuetype=None, pt
     _rightlimit = rightlimit
     if valuetype == None :
         print  "默认取值:从json串取除\\n外的所有字符类型字符串"
-        _pattern = _leftlimit  +  "\":(.*?)" +  _rightlimit
+        _pattern = "\":(.*?)"
+        if _leftlimit != None:
+            _pattern = _leftlimit + _pattern
+        if _rightlimit != None:
+            _pattern = _pattern + _rightlimit
     elif valuetype in ['num', 'number', 'NUM', 'Num', 'Number', 'DATA', 'data', 'Data']:
         print  "从json串取数字(包括小数和负数)的字符串"
-        _pattern = _leftlimit  +  "\":\"([-0-9.]+)\"" +  _rightlimit
+        _pattern = "\":\"([-0-9.]+)\""
+        if _leftlimit != None:
+            _pattern = _leftlimit + _pattern
+        if _rightlimit != None:
+            _pattern = _pattern + _rightlimit
     elif valuetype in ['int', 'INT', 'Int', 'Long', 'long', 'LONG']:
         print  "从json串取整型（数字）的字符串"
-        _pattern = _leftlimit  +  "\":([0-9]+)" +  _rightlimit
+        _pattern = "\":([0-9]+)"
+        if _leftlimit != None:
+            _pattern = _leftlimit + _pattern
+        if _rightlimit != None:
+            _pattern = _pattern + _rightlimit
     elif valuetype in ['char', 'string', 'str', 'Char', 'Str', 'CHAR', 'STR']:
         print  "从json串取字母类型的字符串"
-        _pattern = _leftlimit  +  "\":\"([a-zA-Z]+)\"" +  _rightlimit
+        _pattern = "\":\"([a-zA-Z]+)\""
+        if _leftlimit != None:
+            _pattern = _leftlimit + _pattern
+        if _rightlimit != None:
+            _pattern = _pattern + _rightlimit
     elif valuetype in ['url', 'URL', 'Url']:
         print  "从json串取URL类型字符串"
-        _pattern = _leftlimit  +  "\":\"([a-zA-z]+://[^\s]+?)\"" +  _rightlimit
+        _pattern =  "\":\"([a-zA-z]+://[^\s]+?)\""
+        if _leftlimit != None:
+            _pattern = _leftlimit + _pattern
+        if _rightlimit != None:
+            _pattern = _pattern + _rightlimit
     elif valuetype in ['gen', 'GEN', 'Gen']:
         print  "通用取值除\\n外所有字符，支持非json字符串取值"
-        _pattern = _leftlimit   +  "(.*?)" +  _rightlimit
+        _pattern = "(.*?)"
+        if _leftlimit!=None:
+            _pattern = _leftlimit + _pattern
+        if _rightlimit!=None:
+            _pattern =  _pattern + _rightlimit
     else:
         print  "未匹配到类型，为自定义类型,匹配的规则为", pt
-        _pattern = _leftlimit  +  pt  +  _rightlimit
+        _pattern = pt
+        if _leftlimit != None:
+            _pattern = _leftlimit + _pattern
+        if _rightlimit != None:
+            _pattern = _pattern + _rightlimit
     print "正则表达示：", _pattern
     _compile_pattern = re.compile(_pattern)
     print "编译的正则表达示为", _compile_pattern.pattern
-    returnList = re.findall(_compile_pattern, yuanstring, flags=0)
+    returnList = re.findall(_compile_pattern, yuanstring, 0)
     print "符合条件的信息列表:", returnList
     listlen = len(returnList)
     print "符合条件共:", listlen
@@ -95,7 +143,7 @@ def FindAndValue(yuanstring, leftlimit, rightlimit, ORD=None, valuetype=None, pt
 
 def Dict_sorted(scrdict,type=None):
     '''
-    :param scrdict: 源字典类型的参数；
+    :param scrdict: 源字典类型的参数；/r/n
     :param type: 排序类型，None为不排序返回；1为key的升序排序，2代表按key的降序排序，3代表按value的升序排序，4代表按value的降序排序，其他不排序返回列表嵌套列表
     :return: 返回列表类型参数；匹配到返回列表嵌套元组[(key1,value1),(key2,value2)...]；未匹配到返回列表嵌套列表[[key1,value1],[key2,value2]...]
     '''
@@ -128,10 +176,13 @@ def Dict_sorted(scrdict,type=None):
     return  orderlist
 
 def Json_to_Dict(scrdata):
+    """
+    :param scrdata类型为dict时，以json字符串输出，类型为json字符串时，以字典类型输出；
+    """
     #print   type.__doc__
     if  type(scrdata)==unicode:
         scrdata = scrdata.decode('unicode_escape')
-    print   "scrdata类型为%s".decode('utf-8') %type(scrdata)
+    print   "scrdata类型为%s".decode('UTF-8') %type(scrdata)
     if type(scrdata)==dict:
         print "源变量为dict类型,将转为字符str类型"
         DictJson = json.dumps(scrdata)
@@ -147,13 +198,158 @@ def Json_to_Dict(scrdata):
         DictJson=scrdata
     return DictJson
 
+
+def funfilter(srcobj,conditionstr):
+    print  "funfilter start ..."
+    conditionlist = list()
+    condition = conditionstr
+    if "And" in conditionstr:
+        conditionlist = conditionstr.split(" And ")
+    elif "OR" in conditionstr:
+        conditionlist = conditionstr.split(" OR ")
+    else:
+        conditionlist[0] = conditionstr
+    print "条件列表为："
+    print conditionlist
+    i=1
+    for  condition in conditionlist:
+        #(yuanstring, leftlimit, rightlimit, ORD=None, valuetype=None, pt=None):
+        if "==" in condition:
+            filter = FindAndValue(condition,None,"==",1,"Gen")
+        elif "<=" in condition:
+            filter =   FindAndValue(condition,None,"<=",1,"Gen")
+        elif ">=" in condition:
+            filter =  FindAndValue(condition,None,">=",1,"Gen")
+        elif "<>" in condition:
+            filter = FindAndValue(condition,None,"<>",1,"Gen")
+        elif "!=" in condition:
+            filter =  FindAndValue(condition,None,"!=",1,"Gen")
+        elif "<" in condition:
+            filter =  FindAndValue(condition,None,"<",1,"Gen")
+        elif ">" in condition:
+            filter =  FindAndValue(condition,None,">",1,"Gen")
+        else:
+            filter = "1=1"
+        filter = filter.replace("\"","")
+        print  u"条件解析的第%s个条件为%s" % (i,condition)
+        #print  "condition=",condition
+        print   "字段为",filter
+        i=i+1
+        if type(srcobj)==list:
+            objlen= len(srcobj)
+            if objlen<=0:
+                print "没有符合要求的数据"
+                srcobj=list()
+            else:
+                print  "objlen=",objlen
+                korder = 0
+                print "korderN进入for之前=",korder
+                for indexN in range(0,objlen):
+                    print  "indexN=",indexN
+                    srcobj_n =  srcobj[korder]
+                    print  srcobj_n
+                    if type(srcobj_n)==dict:
+                        filtersValue = srcobj_n.get(filter)
+                    else:
+                        findValue = FindAndValue(srcobj_n,filter,"[,|}|)]",1)
+                        if findValue==None:
+                            filtersValue="00000"
+                        else:
+                            filtersValue=findValue
+                        filtersValue = filtersValue.replace("\"","")
+                    #print  "filtersValue=", filtersValue
+                    #print   "type(condition)=",type(condition)
+                    #print   "type(filter)=", type(filter)
+                    #print   "type(filtersValue)=", type(filtersValue)
+                    if type(filtersValue)==int:
+                        #当值为int类型时，不能用replace来处理，要先转为字符类型；
+                        filtersValue=str(filtersValue)
+                    conditionx = condition.replace(filter,filtersValue)
+                    print  "conditionx=", conditionx
+                    print  "eval(conditionx)=",eval(conditionx)
+                    if  eval(conditionx)==False:
+                        print  "不符合条件%s，移除此项"   %condition
+                        print  "处理前：%s"  %srcobj
+                        srcobj.remove(srcobj_n)
+                        print "列表取值序号korder保持不变=",korder
+                        print  u"第%s轮的%d次scrobj处理后：%s"  %(i-1,indexN+1,srcobj)
+                    else:
+                        korder = korder + 1
+                        print "列表取值序号korder增加1=", korder
+                        print  "%s 符合条件"  %srcobj_n
+                        print   "condition为",condition
+                        print  u"第%s轮的%d次scrobj为%s"  %(i-1,indexN+1,srcobj)
+
+        else:
+            print  "请将源数据转换为列表格式"
+    print  "条件表达式为",conditionstr
+    print  "funfilter返回的结果为",srcobj
+    print  "funfilter End ..."
+    return  srcobj
+
+    
+
+
+def pyfilter(srcobjs,condition,spitstring=None):
+    """
+
+    :param srcobj: 原数据，可以为字符类型和列表类型 因RF输入的参数都为Unicode类型，在RF中引用要用list,str强转类型； /r/n
+
+    :param condition: 过滤条件,支持==,>=,<=,<>,!=,>,<,连接符暂只支持And; 比如 a>10 And b!=0 And "C"=="abcdef"  /r/n
+
+    :param spitstring: 当srcobj为字符类型，此字段为分割符或分割字符,默认为},{   ; srcobj为dict不用传,/r/n
+
+    :return: 返回list,无结果返回空list/r/n
+
+    例如：/r/n
+
+        e="[{\"b\":7,\"c\":\"ab\",\"a\":1},{\"b\":9,\"c\":\"4a6\",\"a\":12},{\"b\":9,\"c\":\"4a6\",\"a\":112}]"   /r/n
+
+        d = pyfilter(e,"b>=9 And \"c\"==\"4a6\" And a<>112","c")
+
+    """
+    print  "pyfilter start ..."
+    srcobj =  srcobjs
+    print  type(srcobjs)
+    if type(srcobj)==list:
+        resrobj = funfilter(srcobj,condition)
+    elif type(srcobj)==str:
+        if spitstring==None:
+            spitstrings = "}Joyrunzzqsplitstring{"
+            if "}, {" in srcobj:
+                srcobj = srcobj.replace("}, {", spitstrings)
+            if  "},{"  in  srcobj:
+                srcobj = srcobj.replace("},{", spitstrings)
+        else:
+            spitstrings= spitstring + "Joyrunzzqsplitstring" + spitstring
+            srcobj = srcobj.replace(spitstring,spitstrings)
+        srclist = srcobj.split("Joyrunzzqsplitstring")
+        print "切割前的srcobj为".decode("UTF-8"),srcobj
+        print 'srclist列表为'.decode("UTF-8"), srclist
+        resrobj = funfilter(srclist,condition)
+    else:
+        resrobj="请将原数据转为字符类型或列表类型".decode("UTF-8")
+    print  "pyfilter结果为:".decode("UTF-8"),resrobj
+    print  "pyfilter End ..."
+    return  resrobj
+
+
+
+
+
+
 # 测试代码
 if __name__ == '__main__':
     #t = tools_reptile()
-   # a = FindAndValue("[{\"ret\":11111},{\"ret\":2222},{\"ret\":\"ab1cd_w\"}]","ret","}",'1')
+    #a = FindAndValue("[{\"ret\":11111},{\"ret\":2222},{\"ret\":\"ab1cd_w\"}]","ret","}",'1',"int")
     #b = Dict_sorted({"ret":"0","sid":"e8705bd983da43d98b9cc0963a78666d","lasttime":1533108241,"user":12345,"msg":"e1aa"})
     #data1 = "{\"b\":789,\"c\": 456,\"a\":123}"
-    data1 = {"b":789,"c": 456,"a":123}
-    c= Json_to_Dict(data1)
+    #data1 = {"b":789,"c": 456,"a":123}
+    #c= Json_to_Dict(data1)
+    #print  str.split.__doc__
+    #e = [{"b":7,"c":"ab","a":1},{"b":9,"c":"4a6","a":12},{"b":78,"c":46,"a":13},{"b":781,"c":"416","a":123},{"b":7,"c":"6","a":913},{"b":78,"c":"46","a":93},{"b":79,"c":"56","a":13}]
+    e="[{\"b\":7,\"c\":\"ab\",\"a\":1},{\"b\":9,\"c\":\"4a6\",\"a\":12},{\"b\":9,\"c\":\"4a6\",\"a\":112}]"
+    d = pyfilter(e,"b>=9 And \"c\"==\"4a6\" And a<>112","c")
+    
 
     #print Dict_sorted.__doc__,pyfilter.__doc__
