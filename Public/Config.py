87dd5154f249953d567d2e0e66970b37766c98c9
South <caelyntang@163.com>
2018-08-17 09:34:22 +0800

    修改小程序登录失效

--- Public/Config.py
+++ Public/Config.py
@@ -189,8 +189,8 @@ if  JoyrunEvn=='Test' or JoyrunEvn=='0':
 	#赛事小程序APPID  根据请求url的path取的第一级目录命名+appid=config中的命名，如赛事小程序trip/,tripappid
 	tripappid='wx1f211a6a3607d8fe'
 	
-	#赛事小程序用户身份sid 2018-6-21   赛事小程序uid15108880，sid
-	tripsid='15c24e792615494383d70f4f2e97b7eaf'
+	#赛事小程序用户身份sid 2018-8-17   赛事小程序uid15108880，sid笙箫默微信
+	tripsid='b4db1909f66a4af3b261a70e875c06a48'
 
 else:
 #=======================线上发布连接地址(Online)========================
