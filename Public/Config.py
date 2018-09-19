a1f910bb2603db6b95936564e266f9eda81c5bbe
South <caelyntang@163.com>
2018-09-19 11:24:48 +0800

    更改版本号3改成4+赛事实况

--- Public/Config.py
+++ Public/Config.py
@@ -39,16 +39,16 @@ SYSVERSION='11.2.1'
 deviceToken='cb9012fd0302813461884e4a3fcc382886b73f6518c1dbe1f031a3901ee72eb4'
 
 #请求头参数-客户端浏览器
-UserAgent='joyRunner/3.1.0 (iPhone; iOS 11.2.1; Scale/3.00)'
+UserAgent='joyRunner/4.1.0 (iPhone; iOS 11.2.1; Scale/3.00)'
 
 #请求头参数-APP的开发版本号
-APPDEVINFO='iOS#3.1.0.01101444'
+APPDEVINFO='iOS#4.1.0.01101444'
 
 #请求头参数-设备类型
 MODELTYPE='iPhone 7 Plus'
 
 #请求头参数-cookies中app的版本号信息
-appversion='ios3.1.1'
+appversion='ios4.1.1'
 
 #请求头参数Content-Type
 ContentType='application/x-www-form-urlencoded'
@@ -74,7 +74,7 @@ appkey1='fb1931e425f84313bfae4b93ab3ccdc4'
 appkey2='1fd6e28fd158406995f77727b35bf20a'
 
 #前端验签名的版本号（需要与appkey1相对应，服务根据这个版本号验证appkey1）
-APPVERSION='100.3.1.0'
+APPVERSION='100.4.1.0'
 
 
 
@@ -165,6 +165,10 @@ if  JoyrunEvn=='Test' or JoyrunEvn=='0':
 	
 	#阿凡达
 	equipment_URL='http://equipment-test.api.thejoyrun.com'
+	
+	#赛事实况
+	racelive_URL='http://racelive-test.api.thejoyrun.com'
+	
 	
 	##===================小程序相关配置=====================
 
@@ -280,6 +284,9 @@ else:
 	
 	#阿凡达
 	equipment_URL='http://equipment.api.thejoyrun.com'	
+	
+	#赛事实况
+	racelive_URL='http://racelive.api.thejoyrun.com'
 	
 	#=====小程序相关配置=================================
 
