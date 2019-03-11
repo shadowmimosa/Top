0e654e4f9163396443fdef376513bea3f2f2ccf4
shadowmimosa <chexuandong@foxmail.com>
2019-03-11 18:39:04 +0800

    Down m3u8

--- Python/reptile/down_mp4/down_m3u8_v2.py
+++ Python/reptile/down_mp4/down_m3u8_v2.py
@@ -3,45 +3,52 @@ def write_file(path, content):
         f.write(content)
         f.flush()
 
-def get_session(pool_connections, pool_maxsize, max_retries):
+
+def get_session(pool_connections=50, pool_maxsize=50, max_retries=3):
     """构造 session"""
-    
-    
-def download(url):
-    import requests, os
+    import requests
     import urllib3
-    from requests.adapters import HTTPAdapter
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    #设置重连次数
+    # requests.adapters.DEFAULT_RETRIES = 15
+    # 设置连接活跃状态为False
+    session = requests.session()
+    session.keep_alive = False
+    session.verify = False
+    session.timeout = (3.05, 5)
+    requests.adapters.HTTPAdapter()
+    adapter = requests.adapters.HTTPAdapter(
+        # pool_connections=pool_connections,
+        # pool_maxsize=pool_maxsize,
+        max_retries=max_retries)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
+
+    return session
+
+
+def download(url):
+    import os
+    import requests.exceptions
 
     file_path = os.path.join(os.path.dirname(__file__), url.split('/')[-2])
     if not os.path.exists(file_path):
         os.mkdir(file_path)
 
-    #设置重连次数
-    requests.adapters.DEFAULT_RETRIES = 15
-    # 设置连接活跃状态为False
-    session = requests.session()
-    session.keep_alive = False
-
-    adapter = HTTPAdapter(max_retries=3)
-    #将重试规则挂载到http和https请求
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-
-    index_m3u8 = session.get(
-        url + "index.m3u8", verify=False).content.decode("utf-8").split('\n')
+    index_m3u8 = session.get(url +
+                             "index.m3u8").content.decode("utf-8").split('\n')
     for value in index_m3u8:
         if "index" in value:
             size = 0
             num = 0
             print("---> Downloading {} now.".format(value.replace('.ts', '')))
             try:
-                resp = session.get(
-                    url + value, stream=True, verify=False, timeout=(3.05, 5))
+                resp = session.get(url + value, stream=True)
             except requests.exceptions.ChunkedEncodingError:
-                resp = session.get(url + value, stream=True, verify=False)
+                resp = session.get(url + value, stream=True)
             except requests.exceptions.Timeout:
-                resp = session.get(url + value, stream=True, verify=False)
+                resp = session.get(url + value, stream=True)
             else:
                 with open(os.path.join(file_path, value), 'wb') as mp4:
                     try:
@@ -54,14 +61,21 @@ def download(url):
                     except requests.exceptions.ConnectionError:
                         print("The {} is downloaded fail\n".format(value))
                         continue
+                    except requests.exceptions.ChunkedEncodingError:
+                        print("The {} is downloaded fail\n".format(value))
+                        continue
                     print(
                         "The {} is down.\nThe size is {}, and writing times is {}.\n"
                         .format(value.replace('.ts', ''), size, num))
 
+
 def main():
+    """The function of main"""
     download("https://uuxo5.com/videos/5beb9dce9c68d1a465fe122b/")
 
+
 if __name__ == "__main__":
+    session = get_session()
     main()
     # url = "https://uuxo8.com/videos/5bb88e4b5d4b454c7e8d415d/"
     url = "https://uuxo5.com/videos/5beb9dce9c68d1a465fe122b/"
