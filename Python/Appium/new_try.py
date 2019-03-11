657d6528efdf93334299dac30166e7507babe971
shadowmimosa <chexuandong@foxmail.com>
2019-03-11 18:38:33 +0800

    try Appium

---
+++ Python/Appium/new_try.py
@@ -0,0 +1,28 @@
+# from appium import webdriver
+
+# DESIRED_CAPS = {}
+# DESIRED_CAPS['platformName'] = 'Android'
+# DESIRED_CAPS['deviceName'] = 'emulator-5554'
+# DESIRED_CAPS['platformVersion'] = '9'
+# # DESIRED_CAPS['deviceName'] = 'd19b1585'
+# # DESIRED_CAPS['appPackage'] = 'com.taobao.taobao'
+# # DESIRED_CAPS['appActivity'] = 'com.taobao.tao.welcome.Welcome'
+
+# DRIVER = webdriver.Remote('http://localhost:4723/wd/hub', DESIRED_CAPS)
+
+# # DRIVER.find_element_by_name("1").click()
+# # DRIVER.find_element_by_name("5").click()
+# # DRIVER.find_element_by_name("9").click()
+# # DRIVER.find_element_by_name("delete").click()
+# # DRIVER.find_element_by_name("9").click()
+# # DRIVER.find_element_by_name("5").click()
+# # DRIVER.find_element_by_name("+").click()
+# # DRIVER.find_element_by_name("6").click()
+# # DRIVER.find_element_by_name("=").click()
+
+# DRIVER.quit()
+
+import uiautomator2 as u2
+
+device = u2.connect('10.99.1.93')
+print(device.info)
